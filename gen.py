b736974ebdf84685537c7c2f6e16d8abc155a7d3
Zachary Talis <zacharytalis@gmail.com>
2023-11-04 15:52:56 -0400

    Styling and file niceties

--- gen.py
+++ gen.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python3
 
-defs = open("defs.txt", "r", 1, "utf-8")
+defs = open("defs.txt", "r", encoding="utf-8")
 lines = defs.readlines()
+defs.close()
+
 functions = {}
 i = 0
 mostRecentFunction = ""
+
 for line in lines:
     text = line[0:-1]
 
     match ["function", "signature", "description", "space"][i % 4]:
         case "function":
             mostRecentFunction = text
-            functions[mostRecentFunction] = {"function": text, "signature": "", "description": ""}
+            functions[mostRecentFunction] = {
+                "function": text,
+                "signature": "",
+                "description": "",
+            }
         case "signature":
-            if (functions[mostRecentFunction]["signature"] == ""):
+            if functions[mostRecentFunction]["signature"] == "":
                 functions[mostRecentFunction]["signature"] = text
         case "description":
-            if (functions[mostRecentFunction]["description"] == ""):
-                functions[mostRecentFunction]["description"] = text.replace("\\", "\\\\")
-                functions[mostRecentFunction]["description"] = functions[mostRecentFunction]["description"].replace("\"", "\\\"")
+            if functions[mostRecentFunction]["description"] == "":
+                functions[mostRecentFunction]["description"] = text.replace(
+                    "\\", "\\\\"
+                )
+                functions[mostRecentFunction]["description"] = functions[
+                    mostRecentFunction
+                ]["description"].replace('"', '\\"')
 
     i += 1
 defs.close()
@@ -26,20 +37,19 @@ defs.close()
 outputString = "{\n\t///// BEGIN TF2 VScript Snippets /////\n"
 sortedKeys = sorted(list(functions.keys()))
 for key in sortedKeys:
-
     prefix, body = "", ""
     baseSignature = functions[key]["signature"]
     parenIndex = baseSignature.find("(")
-    if (parenIndex >= 0):
+    if parenIndex >= 0:
         prefix = baseSignature[0:parenIndex].split(" ")[-1]
         # We need to parse params
-        params = baseSignature[parenIndex+1:-1].split(", ")
+        params = baseSignature[parenIndex + 1 : -1].split(", ")
         body = prefix + "("
         i = 1
-        if (params[0] != ""):
+        if params[0] != "":
             for param in params:
                 body += "${" + str(i) + ":" + param + "}"
-                if (i != len(params)):
+                if i != len(params):
                     body += ", "
                 i += 1
         body += ")$0"
@@ -47,9 +57,22 @@ for key in sortedKeys:
         # This is a constant
         prefix, body = baseSignature, baseSignature + "$0"
 
-    outputString += ("\t\"" + baseSignature + "\": {\n\t\t\"prefix\": \"" + prefix + "\",\n\t\t\"body\": [\n\t\t\t\"" + body + "\"\n\t\t],\n\t\t\"description\": \"" + functions[key]["description"] + "\"\n\t},\n")
-outputString = outputString[:len(outputString)-2] + "\n\t///// END TF2 VScript Snippets /////\n}"
-print(outputString, end="")
+    outputString += (
+        '\t"'
+        + baseSignature
+        + '": {\n\t\t"prefix": "'
+        + prefix
+        + '",\n\t\t"body": [\n\t\t\t"'
+        + body
+        + '"\n\t\t],\n\t\t"description": "'
+        + functions[key]["description"]
+        + '"\n\t},\n'
+    )
+outputString = (
+    outputString[: len(outputString) - 2]
+    + "\n\t///// END TF2 VScript Snippets /////\n}"
+)
 
 outputFile = open("squirrel.json", "w")
 outputFile.write(outputString)
+outputFile.close()
