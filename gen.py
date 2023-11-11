425275f58c0d487aecfc0ebe5784d506e893bd97
dani <35577693+dangreene0@users.noreply.github.com>
2023-11-11 18:43:39 -0500

    Changed outputString into a dict
    - Got rid of outputString all together.
    - Refactored main function
    - Updated file management

--- gen.py
+++ gen.py
@@ -1,80 +1,78 @@
 #!/usr/bin/env python3
+from json import dump, load
 
+def main():
+    # TODO add logic for OnGameEvent so that the end result is "OnGameEvent_${1:name}(${2:table params})$0"
+    # Change data management to dicts and json
+    with open("defs.txt", "r", encoding="utf-8") as defs_file:
+        defs_data = defs_file.readlines()
 
-# TODO add logic for OnGameEvent so that the end result is "OnGameEvent_${1:name}(${2:table params})$0"
-defs = open("defs.txt", "r", encoding="utf-8")
-lines = defs.readlines()
-defs.close()
+    functions = {}
+    i = 0
+    most_recent_function = ""
 
-functions = {}
-i = 0
-mostRecentFunction = ""
+    for line in defs_data:
+        text = line[0:-1]
 
-for line in lines:
-    text = line[0:-1]
+        match ["function", "signature", "description", "space"][i % 4]:
+            case "function":
+                most_recent_function = text
+                functions[most_recent_function] = {
+                    "function": text,
+                    "signature": "",
+                    "description": "",
+                }
+            case "signature":
+                if functions[most_recent_function]["signature"] == "":
+                    functions[most_recent_function]["signature"] = text
+            case "description":
+                if functions[most_recent_function]["description"] == "":
+                    functions[most_recent_function]["description"] = text.replace(
+                        "\\", "\\\\"
+                    )
+                    functions[most_recent_function]["description"] = functions[
+                        most_recent_function
+                    ]["description"].replace('"', '\\"')
 
-    match ["function", "signature", "description", "space"][i % 4]:
-        case "function":
-            mostRecentFunction = text
-            functions[mostRecentFunction] = {
-                "function": text,
-                "signature": "",
-                "description": "",
+        i += 1
+
+    sorted_keys = sorted(list(functions.keys()))
+    function_registry = {}
+
+    for key in sorted_keys:
+        prefix, body = "", ""
+        base_signature = functions[key]["signature"]
+        paren_index = base_signature.find("(")
+        if paren_index >= 0:
+            prefix = base_signature[0:paren_index].split(" ")[-1]
+            # We need to parse params
+            params = base_signature[paren_index + 1 : -1].split(", ")
+            body = prefix + "("
+            i = 1
+            if params[0] != "":
+                for param in params:
+                    body += "${" + str(i) + ":" + param + "}"
+                    if i != len(params):
+                        body += ", "
+                    i += 1
+            body += ")$0"
+        else:
+            # This is a constant
+            prefix, body = base_signature, base_signature + "$0"
+        
+        function_entry = {
+            base_signature: {
+                "prefix": prefix,
+                "body": [
+                    body
+                ],
+                "description" : functions[key]["description"]
             }
-        case "signature":
-            if functions[mostRecentFunction]["signature"] == "":
-                functions[mostRecentFunction]["signature"] = text
-        case "description":
-            if functions[mostRecentFunction]["description"] == "":
-                functions[mostRecentFunction]["description"] = text.replace(
-                    "\\", "\\\\"
-                )
-                functions[mostRecentFunction]["description"] = functions[
-                    mostRecentFunction
-                ]["description"].replace('"', '\\"')
+        }
+        function_registry.update(function_entry)
 
-    i += 1
-defs.close()
+    with open("squirrel_.json", "w") as json_test:
+        dump(function_registry, json_test, indent=2)
 
-outputString = "{\n\t///// BEGIN TF2 VScript Snippets /////\n"
-sortedKeys = sorted(list(functions.keys()))
-for key in sortedKeys:
-    prefix, body = "", ""
-    baseSignature = functions[key]["signature"]
-    parenIndex = baseSignature.find("(")
-    if parenIndex >= 0:
-        prefix = baseSignature[0:parenIndex].split(" ")[-1]
-        # We need to parse params
-        params = baseSignature[parenIndex + 1 : -1].split(", ")
-        body = prefix + "("
-        i = 1
-        if params[0] != "":
-            for param in params:
-                body += "${" + str(i) + ":" + param + "}"
-                if i != len(params):
-                    body += ", "
-                i += 1
-        body += ")$0"
-    else:
-        # This is a constant
-        prefix, body = baseSignature, baseSignature + "$0"
-
-    outputString += (
-        '\t"'
-        + baseSignature
-        + '": {\n\t\t"prefix": "'
-        + prefix
-        + '",\n\t\t"body": [\n\t\t\t"'
-        + body
-        + '"\n\t\t],\n\t\t"description": "'
-        + functions[key]["description"]
-        + '"\n\t},\n'
-    )
-outputString = (
-    outputString[: len(outputString) - 2]
-    + "\n\t///// END TF2 VScript Snippets /////\n}"
-)
-
-outputFile = open("squirrel.json", "w")
-outputFile.write(outputString)
-outputFile.close()
+if __name__ == "__main__":
+    main()
